712bf98e9ac4555333ff36a6ef6d4e16dfc4b1ba
Your Name <you@example.com>
2020-11-17 22:08:25 +0300

    error2

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+int main() {
+	std::ifstream in_file("in.json");
+	json j;
+	in_file >> j;
+	
+	json j2 = json::array();
+	j2.push_back({ {"userId", 1}, {"task_completed" , 10} });
+	j2.push_back({ {"userId", 1}, {"task_completed" , 10} });
+	j2.push_back({ {"userId", 1}, {"task_completed" , 10} });
+
+	std::cout << j2.dump(2);
+}
---
+++ ConsoleApplication5/ConsoleApplication5/Header.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+
+
+int i = 10;
